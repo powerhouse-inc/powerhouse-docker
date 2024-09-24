914db039ab86aa731456abec9b98c4c3236e30a1
Frank <frank.pfeift@protonmail.com>
2024-09-24 12:43:52 +0200

    feat: added docker pull before executing actual command

--- cli.sh
+++ cli.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-docker compose  --env-file environments/.env.$1 --project-name $1 -f compose/apps.connect.yml -f compose/apps.switchboard.yml -f compose/services.yml -f compose/monitoring.yml $2
+docker compose  --env-file environments/.env.$1 --project-name $1 -f compose/apps.connect.yml -f compose/apps.switchboard.yml -f compose/services.yml -f compose/monitoring.yml pull
+
+docker compose  --env-file environments/.env.$1 --project-name $1 -f compose/apps.connect.yml -f compose/apps.switchboard.yml -f compose/services.yml -f compose/monitoring.yml $2 $3 $4 $5 $6 $7 $8 $9
