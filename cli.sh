548ef9af1a3481074fe24e26a1621df5b87565eb
Frank <frank.pfeift@protonmail.com>
2024-09-24 12:33:27 +0200

    fix: updated command

--- cli.sh
+++ cli.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker compose  --env-file environments/.env.$2 --project-name $2 -f compose/apps.connect.yml -f compose/apps.switchboard.yml -f compose/services.yml -f compose/monitoring.yml $1 $3
+docker compose  --env-file environments/.env.$1 --project-name $1 -f compose/apps.connect.yml -f compose/apps.switchboard.yml -f compose/services.yml -f compose/monitoring.yml $2
